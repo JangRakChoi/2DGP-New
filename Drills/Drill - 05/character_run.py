756596187183228f57ec9d259bb2b9abbfc361b9
JangRakChoi(laptop) <bisu235@naver.com>
2018-09-17 19:03:22 +0900

    Drill - 05 스텝9 y축 이동, 정지

--- Drills/Drill - 05/character_run.py
+++ Drills/Drill - 05/character_run.py
@@ -222,6 +222,18 @@ def MoveninthStep() :
         delay(0.01)
         get_events()
 
+    while (y < 349) :
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, 0, 100, 100, x, y)
+        update_canvas()
+        frame = (frame + 1) % 8
+        y += 2
+        delay(0.01)
+        get_events()
+    
+    StopLeftSide(x, y)
+
 def MovetenthStep() :
     pass
 
