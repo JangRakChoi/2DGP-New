056050d70696880be3c15f2a44fc0871a7416f3b
JangRakChoi(laptop) <bisu235@naver.com>
2018-09-17 18:36:29 +0900

    Drill - 05 뼈대

---
+++ Drills/Drill - 05/character_run.py
@@ -0,0 +1,54 @@
+from pico2d import *
+open_canvas()
+grass = load_image('grass.png')
+character = load_image('animation_sheet.png')
+
+def StopRightSide(x, y) :
+    pass
+
+def StopLeftSide(x, y) :
+    pass
+
+def MoveFirstStep() :
+    pass
+
+def MoveSecondStep() :
+    pass
+
+def MoveThirdStep() :
+    pass
+
+def MoveFourthStep() :
+    pass
+
+def MoveFifthStep() :
+    pass
+
+def MoveSixthStep() :
+    pass
+
+def MoveSeventhStep() :
+    pass
+
+def MoveEighthStep() :
+    pass
+
+def MoveninthStep() :
+    pass
+
+def MovetenthStep() :
+    pass
+
+while True :
+    MoveFirstStep()
+    MoveSecondStep()
+    MoveThirdStep()
+    MoveFourthStep()
+    MoveFifthStep()
+    MoveSixthStep()
+    MoveSeventhStep()
+    MoveEighthStep()
+    MoveninthStep()
+    MovetenthStep()
+
+close_canvas()
