5f55013982ec402faec5dc94c1001b15ebd1d37c
JangRakChoi(laptop) <bisu235@naver.com>
2018-09-17 18:56:25 +0900

    Drill - 05 스텝6 y축 이동, 정지

--- Drills/Drill - 05/character_run.py
+++ Drills/Drill - 05/character_run.py
@@ -149,6 +149,19 @@ def MoveSixthStep() :
         delay(0.01)
         get_events()
 
+    while (y > 92) :
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, 100, 100, 100, x, y)
+        update_canvas()
+        frame = (frame + 1) % 8
+        y -= 5
+        delay(0.01)
+        get_events()
+
+    StopRightSide(x, y)
+
+
 def MoveSeventhStep() :
     pass
 
