19d934ee954780c7c5121aa011ee7d45e6b4aa32
JangRakChoi(laptop) <bisu235@naver.com>
2018-09-17 18:50:42 +0900

    Drill - 05 스텝4 y축 이동, 정지

--- Drills/Drill - 05/character_run.py
+++ Drills/Drill - 05/character_run.py
@@ -101,6 +101,18 @@ def MoveFourthStep() :
         delay(0.01)
         get_events()
 
+    while (y < 225) :
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, 0, 100, 100, x, y)
+        update_canvas()
+        frame = (frame + 1) % 8
+        y += 5
+        delay(0.01)
+        get_events()
+
+    StopLeftSide(x, y)
+
 def MoveFifthStep() :
     pass
 
