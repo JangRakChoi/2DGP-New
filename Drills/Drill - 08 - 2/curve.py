4217941202cf4079782c6f1fe1cd44f5535cc4dd
JangRakChoi(laptop) <bisu235@naver.com>
2018-10-08 15:06:56 +0900

    Drill - 08-2 p5-p6 연결

--- Drills/Drill - 08 - 2/curve.py
+++ Drills/Drill - 08 - 2/curve.py
@@ -60,4 +60,14 @@ while running :
         y = ((-t ** 3 + 2 * t ** 2 - t) * yPos[2] + (3 * t ** 3 - 5 * t ** 2 + 2) * yPos[3] + (-3 * t ** 3 + 4 * t ** 2 + t) * yPos[4] + (t ** 3 - t ** 2) * yPos[5]) / 2
         Draw(x, y)
 
+    frame = 0
+    # draw p5-p6
+    for i in range(0, 100, 1):
+        t = i / 100
+        x = ((-t ** 3 + 2 * t ** 2 - t) * xPos[3] + (3 * t ** 3 - 5 * t ** 2 + 2) * xPos[4] + (-3 * t ** 3 + 4 * t ** 2 + t) * xPos[5] + (t ** 3 - t ** 2) * xPos[6]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * yPos[3] + (3 * t ** 3 - 5 * t ** 2 + 2) * yPos[4] + (-3 * t ** 3 + 4 * t ** 2 + t) * yPos[5] + (t ** 3 - t ** 2) * yPos[6]) / 2
+        Draw(x, y)
+
+
+
 close_canvas()
