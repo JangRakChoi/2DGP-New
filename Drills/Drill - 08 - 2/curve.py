345917764da418fead3edd9fe3e7869780b1a993
JangRakChoi(laptop) <bisu235@naver.com>
2018-10-08 15:07:55 +0900

    Drill - 08-2 p7-p8 연결

--- Drills/Drill - 08 - 2/curve.py
+++ Drills/Drill - 08 - 2/curve.py
@@ -77,6 +77,12 @@ while running :
         y = ((-t ** 3 + 2 * t ** 2 - t) * yPos[4] + (3 * t ** 3 - 5 * t ** 2 + 2) * yPos[5] + (-3 * t ** 3 + 4 * t ** 2 + t) * yPos[6] + (t ** 3 - t ** 2) * yPos[7]) / 2
         Draw(x, y)
 
-
+    frame = 0
+    # draw p7-p8
+    for i in range(0, 100, 1):
+        t = i / 100
+        x = ((-t ** 3 + 2 * t ** 2 - t) * xPos[5] + (3 * t ** 3 - 5 * t ** 2 + 2) * xPos[6] + (-3 * t ** 3 + 4 * t ** 2 + t) * xPos[7] + (t ** 3 - t ** 2) * xPos[8]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * yPos[5] + (3 * t ** 3 - 5 * t ** 2 + 2) * yPos[6] + (-3 * t ** 3 + 4 * t ** 2 + t) * yPos[7] + (t ** 3 - t ** 2) * yPos[8]) / 2
+        Draw(x, y)
 
 close_canvas()
