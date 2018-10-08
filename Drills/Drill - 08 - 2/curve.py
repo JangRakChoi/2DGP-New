38ff782cca419ea516936cefdff56d4a189a67a2
JangRakChoi(laptop) <bisu235@naver.com>
2018-10-08 15:01:16 +0900

    Drill - 08-2 draw함수

--- Drills/Drill - 08 - 2/curve.py
+++ Drills/Drill - 08 - 2/curve.py
@@ -13,9 +13,20 @@ xPos = [random.randint(0, 1280 - 20) for n1 in range(10)]
 yPos = [random.randint(0, 1024 - 20) for n2 in range(10)]
 
 def Draw(x, y) :
-    pass
+    global frame
+    global temp
+    if (temp < x):
+        character.clip_draw(frame * 100, 100, 100, 100, x, y)
+    else:
+        character.clip_draw(frame * 100, 0, 100, 100, x, y)
+    update_canvas()
+    frame = (frame + 1) % 8
+    clear_canvas()
+    kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+    temp = x
+    delay(0.05)
 
 while running :
-    pass
+    Draw(100, 100)
 
 close_canvas()
