8fbdebb9f1215b703407eda0c831fb9a04013ee5
JangRakChoi(laptop) <bisu235@naver.com>
2018-10-08 14:34:27 +0900

    Drill - 08-1 p1 - p2 연결

--- Drills/Drill - 08 - 1/curve.py
+++ Drills/Drill - 08 - 1/curve.py
@@ -56,7 +56,14 @@ def draw_curve_5_points(p1, p2, p3, p4, p5):
     draw_big_point(p4)
     draw_big_point(p5)
 
-    
+    # draw p1-p2
+    for i in range(0, 50, 1):
+        t = i / 100
+        x = (2 * t ** 2 - 3 * t + 1) * p1[0] + (-4 * t ** 2 + 4 * t) * p2[0] + (2 * t ** 2 - t) * p3[0]
+        y = (2 * t ** 2 - 3 * t + 1) * p1[1] + (-4 * t ** 2 + 4 * t) * p2[1] + (2 * t ** 2 - t) * p3[1]
+        draw_point((x, y))
+    draw_point(p2)
+
 
 prepare_turtle_canvas()
 
