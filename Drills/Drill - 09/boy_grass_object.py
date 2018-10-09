f9cfe622c96fc3883e73fcadd9f80c219a106754
JangRakChoi(laptop) <bisu235@naver.com>
2018-10-09 17:02:40 +0900

    Drill - 09 공추가

--- Drills/Drill - 09/boy_grass_object.py
+++ Drills/Drill - 09/boy_grass_object.py
@@ -1,4 +1,5 @@
 from pico2d import *
+import random
 
 class Grass :
     def __init__(self):
@@ -19,6 +20,32 @@ class Boy:
 
     def draw(self):
         self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, self.y)
+
+class Ball:
+    def __init__(self):
+        self.x, self.y = random.randint(10, 780), 700
+        self.ball_size = random.randint(0, 1)
+        self.speed = random.randint(5, 20)
+
+        if self.ball_size == 0:
+            self.image = load_image('ball21x21.png')
+        elif self.ball_size == 1:
+            self.image = load_image('ball41x41.png')
+
+    def update(self):
+        if self.ball_size == 0:
+            if self.y <= 65:
+                self.y = 65
+            else:
+                self.y -= self.speed
+        elif self.ball_size == 1:
+            if self.y <= 75:
+                self.y = 75
+            else:
+                self.y -= self.speed
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
 
 def handle_events():
     global running
