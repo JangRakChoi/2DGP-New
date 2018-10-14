b3c7860be0453bd846d925d6bef25264e7d9c9b4
JangRakChoi(laptop) <bisu235@naver.com>
2018-10-15 02:13:09 +0900

    탈북게임 제작 시작

---
+++ 프로젝트/탈북게임/탈북게임.py
@@ -0,0 +1,23 @@
+from pico2d import *
+import random
+
+class Map :
+    pass
+
+class Player:
+    pass
+
+class Enemy:
+    pass
+
+def handle_events():
+    pass
+
+open_canvas()
+
+running = True
+
+while running:
+    pass
+
+close_canvas()
