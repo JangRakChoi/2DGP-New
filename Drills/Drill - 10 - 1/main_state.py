0651ab0cdb9400ec2e36440178ee8f466d53cbd4
JangRakChoi(laptop) <bisu235@naver.com>
2018-10-15 21:28:42 +0900

    Drill-10-1 pause 구현

--- Drills/Drill - 10 - 1/main_state.py
+++ Drills/Drill - 10 - 1/main_state.py
@@ -3,6 +3,7 @@ import json
 import os
 
 from pico2d import *
+import pause_state
 
 import game_framework
 import title_state
@@ -75,6 +76,8 @@ def handle_events():
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.change_state(title_state)
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_p :
+            game_framework.push_state(pause_state)
 
 
 def update():
