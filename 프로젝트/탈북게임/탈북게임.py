7600d23fb83babd18b4c77d342c9d30f18de4f7e
JangRakChoi(laptop) <bisu235@naver.com>
2018-10-15 22:10:50 +0900

    탈북게임 맵
    20181015

--- 프로젝트/탈북게임/탈북게임.py
+++ 프로젝트/탈북게임/탈북게임.py
@@ -1,8 +1,13 @@
 from pico2d import *
 import random
 
+open_canvas()
+
 class Map :
-    pass
+    def __init__(self) :
+        self.image = load_image('map3.png')
+    def draw(self) :
+        self.image.draw(300, 300)
 
 class Player:
     pass
@@ -13,11 +18,16 @@ class Enemy:
 def handle_events():
     pass
 
-open_canvas()
+def update():
+    pass
 
+player = Player()
+background = Map()
 running = True
 
 while running:
-    pass
+    background.draw()
+    update_canvas()
+    delay(0.05)
 
 close_canvas()
