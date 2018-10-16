07d477246b97ba915509c07731a9a72218570317
JangRakChoi(laptop) <bisu235@naver.com>
2018-10-16 20:24:53 +0900

    탈북게임 캐릭터

--- 프로젝트/탈북게임/탈북게임.py
+++ 프로젝트/탈북게임/탈북게임.py
@@ -9,11 +9,26 @@ class Map :
     def draw(self) :
         self.image.draw(300, 300)
 
-class Player:
-    pass
-
 class Enemy:
-    pass
+    def __init__(self) :
+        self.image = load_image("soldier2.png")
+        # soldier pivot : 50X50
+        self.x, self.y = 100, 100
+        self.frame = 0
+
+    def draw(self) :
+        self.image.draw(self.x, self.y, 100, 100)
+
+class Player:
+    def __init__(self) :
+        self.image = load_image("animation_sheet.png")
+        # soldier pivot : 50X50
+        self.x, self.y = 300, 300
+        self.frame = 0
+
+    def draw(self) :
+        self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
+
 
 def handle_events():
     pass
@@ -22,11 +37,15 @@ def update():
     pass
 
 player = Player()
+enemy = Enemy()
 background = Map()
 running = True
 
 while running:
+    clear_canvas()
     background.draw()
+    player.draw()
+    enemy.draw()
     update_canvas()
     delay(0.05)
 
