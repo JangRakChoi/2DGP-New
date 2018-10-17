954542ae4fc77374d6f0718284bde8233830b5c9
JangRakChoi(laptop) <bisu235@naver.com>
2018-10-17 19:08:10 +0900

    탈북게임 스프라이트 수정 및 적 다수 구현

--- 프로젝트/탈북게임/탈북게임.py
+++ 프로젝트/탈북게임/탈북게임.py
@@ -1,33 +1,33 @@
 from pico2d import *
 import random
 
-open_canvas()
+open_canvas(1920, 1080)
 
 class Map :
     def __init__(self) :
         self.image = load_image('map3.png')
     def draw(self) :
-        self.image.draw(300, 300)
+        self.image.draw(960, 540)
 
 class Enemy:
     def __init__(self) :
-        self.image = load_image("soldier2.png")
+        self.image = load_image("enemy.png")
         # soldier pivot : 50X50
-        self.x, self.y = 100, 100
+        self.x, self.y = random.randint(500, 1800), random.randint(100, 1000)
         self.frame = 0
 
     def draw(self) :
-        self.image.draw(self.x, self.y, 100, 100)
+        self.image.clip_draw(self.frame * 0, 200, 110, 100, self.x, self.y)
 
 class Player:
     def __init__(self) :
-        self.image = load_image("animation_sheet.png")
+        self.image = load_image("player.png")
         # soldier pivot : 50X50
         self.x, self.y = 300, 300
         self.frame = 0
 
     def draw(self) :
-        self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
+        self.image.clip_draw(self.frame, 80, 100, 80, self.x, self.y)
 
 
 def handle_events():
@@ -37,7 +37,7 @@ def update():
     pass
 
 player = Player()
-enemy = Enemy()
+enemies = [Enemy() for n in range(4)]
 background = Map()
 running = True
 
@@ -45,7 +45,8 @@ while running:
     clear_canvas()
     background.draw()
     player.draw()
-    enemy.draw()
+    for enemy in enemies :
+        enemy.draw()
     update_canvas()
     delay(0.05)
 
