d60fd3c84f487fca499d1d2be578afe3be871a03
JangRakChoi(laptop) <bisu235@naver.com>
2018-10-19 18:36:12 +0900

    탈북게임 컨트롤 구현

--- 프로젝트/탈북게임/탈북게임.py
+++ 프로젝트/탈북게임/탈북게임.py
@@ -69,12 +69,32 @@ running = True
 
 def handle_events():
     global running
+    global dirX
+    global dirY
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT:
+                dirX += 1
+            elif event.key == SDLK_UP:
+                dirY += 1
+            elif event.key == SDLK_LEFT:
+                dirX -= 1
+            elif event.key == SDLK_DOWN:
+                dirY -= 1
+            elif event.key == SDLK_ESCAPE:
+                running = False
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                dirX -= 1
+            elif event.key == SDLK_UP:
+                dirY -= 1
+            elif event.key == SDLK_LEFT:
+                dirX += 1
+            elif event.key == SDLK_DOWN:
+                dirY += 1
 
 player = Player()
 enemies = [Enemy() for n in range(4)]
