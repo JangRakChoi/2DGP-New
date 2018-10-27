798d3441a67b851c13183499cb4b3958a075bede
JangRakChoi(laptop) <bisu235@naver.com>
2018-10-27 19:59:11 +0900

    탈북게임 start_state

--- 프로젝트/탈북게임/start_state.py
+++ 프로젝트/탈북게임/start_state.py
@@ -1,1 +1,35 @@
-pass
+from pico2d import *
+import game_framework
+import main_state
+
+name = "StartState"
+image = None
+logo_time = 0.0
+
+def enter():
+    global image
+    image = load_image('start.png')
+def exit():
+    global image
+    del(image)
+
+def update():
+    pass
+
+def draw():
+    global image
+    clear_canvas()
+    image.draw(640, 512)
+    update_canvas()
+
+def handle_events():
+    events = get_events()
+    for event in events :
+        if event.type == SDL_QUIT :
+            game_framework.quit()
+        else :
+            if (event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE) :
+                game_framework.quit()
+            elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_SPACE):
+                game_framework.change_state(main_state)
+
