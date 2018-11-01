ae0b35f4e421e50791aa8102ae7458bfbb834497
JangRakChoi(laptop) <bisu235@naver.com>
2018-11-01 22:26:48 +0900

    탈북게임 failstate

--- 프로젝트/탈북게임/main_state.py
+++ 프로젝트/탈북게임/main_state.py
@@ -1,4 +1,3 @@
-
 import random
 import json
 import os
@@ -6,6 +5,7 @@ import os
 from pico2d import *
 import game_framework
 import game_world
+import FailState
 
 from Player import Player
 from Map3 import Map
@@ -44,6 +44,8 @@ def handle_events():
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.quit()
+        elif player.hp < 0 :
+            game_framework.run(FailState)
         else:
             player.handle_event(event)
 
@@ -51,12 +53,25 @@ def handle_events():
 def update():
     for game_object in game_world.all_objects():
         game_object.update()
+    if collide(player, enemy):
+        player.hp -= 10
 
 def draw():
     clear_canvas()
     for game_object in game_world.all_objects():
         game_object.draw()
     update_canvas()
+
+def collide(a, b) :
+    left_a, bottom_a, right_a, top_a = a.get_bb()
+    left_b, bottom_b, right_b, top_b = b.get_bb()
+
+    if left_a > right_b : return False
+    if right_a < left_b : return False
+    if top_a < bottom_b : return False
+    if bottom_a > top_b : return False
+
+    return True
 
 
 
