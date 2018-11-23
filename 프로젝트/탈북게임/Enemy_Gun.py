ed38f5bdf256991352b1411bbd57e13d9a1e0203
JangRakChoi(laptop) <bisu235@naver.com>
2018-11-23 12:08:59 +0900

    탈북게임 enemy 추가

--- 프로젝트/탈북게임/Enemy_Gun.py
+++ 프로젝트/탈북게임/Enemy_Gun.py
@@ -7,6 +7,7 @@ import random
 import math
 import main_state
 
+
 PIXEL_PER_METER = (10.0 / 0.3)  # 10 pixel 30 cm
 RUN_SPEED_KMPH = 10.0  # Km / Hour
 RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
@@ -18,12 +19,48 @@ TIME_PER_ACTION = 0.5
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
 FRAMES_PER_ACTION = 10
 
-xPos = [600, 400, 500, 700, 900, 1000, 800]
-yPos = [600, 400, 200, 400, 300, 500, 400]
+xPos = [600, 400, 500, 700, 900, 1000, 800, 650, 850]
+yPos = [600, 400, 200, 400, 300, 500, 600, 570, 370]
 
-class Enemy_Knife :
+class Enemy2 :
     def __init__(self) :
-        self.image = load_image("enemy1.png")
+        self.image = load_image("enemy2.png")
+        # soldier pivot : 50X50
+        self.count = random.randint(0, 8)
+        self.x, self.y = random.randint(20, 100) * 10, random.randint(5, 90) * 10
+        self.frame = 0
+        self.nowX, self.nowY, self.n = 0, 0, 0
+        self.velocityRL, self.velocityUD = 0, 0
+        self.event_que = []
+        self.cur_state = WalkState
+        self.cur_state.enter(self, None)
+        self.cur_time = 0
+        self.timer = 0
+        self.n = 0
+        self.stop = random.randint(8, 15)
+        self.where_collide = -1
+
+    def update(self) :
+        self.cur_state.do(self)
+        if len(self.event_que) > 0:
+            event = self.event_que.pop()
+            self.cur_state.exit(self, event)
+            self.cur_state.enter(self, event)
+
+    def draw(self) :
+        self.cur_state.draw(self)
+        draw_rectangle(*self.get_bb())
+
+    def get_bb(self) :
+        return self.x - 40, self.y - 50, self.x + 40, self.y + 50
+
+    def add_event(self, event):
+        self.event_que.insert(0, event)
+
+
+class Enemy_Gun :
+    def __init__(self) :
+        self.image = load_image("enemy2.png")
         # soldier pivot : 50X50
         self.count = random.randint(0, 6)
         self.x, self.y = random.randint(20, 100) * 10, random.randint(5, 90) * 10
@@ -91,6 +128,3 @@ class Enemy_Knife :
 
     def handle_event(self, event):
         pass
-
-
-
